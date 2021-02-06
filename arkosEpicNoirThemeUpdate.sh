7c36d05f1dacd58d8dd0c7ff4091bd1ad3d806d7
Jetup13 <62556437+Jetup13@users.noreply.github.com>
2021-02-05 18:17:23 -0800

    Update Script

--- arkosEpicNoirThemeUpdate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Replace the URL by the correct one for your theme.
-# Theme folder name should match the repo name. 
-sudo wget https://github.com/Jetup13/es-theme-epicnoir/archive/master.zip -O ./update.zip
-sudo unzip -o update.zip -d \..
-sudo rm update.zip
